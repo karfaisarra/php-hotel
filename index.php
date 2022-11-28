09adf056e4a171074ee9d0a0147bbd0f5c251448
karfaisarra <karfaisarra@gmail.com>
2022-11-28 14:30:31 +0100

    add array

---
+++ index.php
@@ -0,0 +1,65 @@
+<?php
+$hotels = [
+
+    [
+        'name' => 'Hotel Belvedere',
+        'description' => 'Hotel Belvedere Descrizione',
+        'parking' => true,
+        'vote' => 4,
+        'distance_to_center' => 10.4
+    ],
+    [
+        'name' => 'Hotel Futuro',
+        'description' => 'Hotel Futuro Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 2
+    ],
+    [
+        'name' => 'Hotel Rivamare',
+        'description' => 'Hotel Rivamare Descrizione',
+        'parking' => false,
+        'vote' => 1,
+        'distance_to_center' => 1
+    ],
+    [
+        'name' => 'Hotel Bellavista',
+        'description' => 'Hotel Bellavista Descrizione',
+        'parking' => false,
+        'vote' => 5,
+        'distance_to_center' => 5.5
+    ],
+    [
+        'name' => 'Hotel Milano',
+        'description' => 'Hotel Milano Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 50
+    ],
+
+];
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <title>PHP Hotel</title>
+</head>
+
+<body>
+    <div class="container my-5">
+        <h1 class="text-center">Hotels</h1>
+        <ul>
+            <?php foreach ($hotels as $hotel) : ?>
+                <li> <?php echo 'Nome Hotel: ' . $hotel['name'] . ' ' . '/ description: ' . $hotel['description'] . ' ' . '/ parking: ' . $hotel['parking'] . ' ' . 'voto: ' . $hotel['vote'] . ' ' . '/ distance_to_center: ' . $hotel['distance_to_center']  ?> </li>
+            <?php endforeach ?>
+        </ul>
+    </div>
+</body>
+
+</html>
